d6d36cb85e39e7c14caca8abfb3149563caa9a4e
qhanw <whenhan@foxmail.com>
2025-08-20 22:06:15 +0800

    feat: adjust env

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
-const hostname = "www.xc01.cc";
+const { STATIC_IMAGE_PROTOCOL, STATIC_IMAGE_HOSTNAME, STATIC_IMAGE_PORT } =
+  process.env;
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -8,28 +9,11 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "127.0.0.1",
-        port: "8081",
+        protocol: (STATIC_IMAGE_PROTOCOL || "https") as "http" | "https",
+        hostname: STATIC_IMAGE_HOSTNAME || "",
+        port: STATIC_IMAGE_PORT,
         pathname: "/**",
       },
-
-      // 主要配置 - 使用环境变量
-      {
-        protocol: (process.env.NEXT_PUBLIC_API_PROTOCOL || "https") as
-          | "http"
-          | "https",
-        hostname: process.env.NEXT_PUBLIC_API_HOST || hostname,
-        port: process.env.NEXT_PUBLIC_API_PORT || "443",
-        pathname: "/**",
-      },
-      // 备用配置 - 带端口
-      { protocol: "https", hostname, port: "443", pathname: "/**" },
-      // 备用配置 - 不带端口
-      { protocol: "https", hostname, pathname: "/**" },
-      // HTTP 配置（备用）
-      { protocol: "http", hostname, port: "80", pathname: "/**" },
-      { protocol: "http", hostname, pathname: "/**" },
     ],
   },
 
