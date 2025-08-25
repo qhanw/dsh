3a3863001b562bc7504d6288566522293c06cf7f
qhanw <whenhan@foxmail.com>
2025-08-25 23:23:25 +0800

    feat: update popular videos

--- src/actions/queryVideoDetails.ts
+++ src/actions/queryVideoDetails.ts
@@ -1,23 +1,28 @@
-import { db } from "@/db";
+import { db, video } from "@/db";
+
+import { sql, eq } from "drizzle-orm";
 
 export async function queryVideoDetails(id: string) {
-  const res = await db.query.video.findFirst({
-    where: (table, { eq }) => eq(table.id, id),
-    columns: {
-      id: true,
-      name: true,
-      enname: true,
-      language: true,
-      period: true,
-      directors: true,
-      actors: true,
-      region: true,
-      image: true,
-      statusStr: true,
-      introduction: true,
-      playLinks: true,
-    },
-  });
+  const res = (
+    await db
+      .update(video)
+      .set({ browse: sql`${video.browse || 0} +1` })
+      .where(eq(video.id, id))
+      .returning({
+        id: video.id,
+        name: video.name,
+        enname: video.enname,
+        language: video.language,
+        period: video.period,
+        directors: video.directors,
+        actors: video.actors,
+        region: video.region,
+        image: video.image,
+        statusStr: video.statusStr,
+        introduction: video.introduction,
+        playLinks: video.playLinks,
+      })
+  )?.[0];
 
   return res
     ? {
