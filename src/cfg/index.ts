a27a82a45f570331380ec817b6ce411d465d365d
qhanw <whenhan@foxmail.com>
2025-08-25 22:54:33 +0800

    feat: adjust site config

--- src/cfg/index.ts
+++ src/cfg/index.ts
@@ -5,7 +5,7 @@ export const SITE_CONFIG = {
   description:
     "忘忧草影视每天搜集分享全网最新无删减好看的电影,2025最新热播vip电视连续剧免费看视频,高清好剧上忘忧草影院网就够了(PC网页端与手机同步,高清免费不卡顿)。",
   keywords: "忘忧草影视,忘忧草影院",
-  domain: "https://www.xc01.cc", // 请替换为实际域名
+  domain: "https://myzhijp.com", // 请替换为实际域名
 };
 
 export const PAGINATION = { page: 1, pageSize: 24 };
